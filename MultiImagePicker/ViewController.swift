510008a017cbe874c224a18b1047ba24b34331c1
Nouman Pervez <nouman.pervez@sword-in.com>
2019-06-19 17:02:52 +0530

    Initial Commit

---
+++ MultiImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiImagePicker
+//
+//  Created by Nouman Pervez on 19/06/19.
+//  Copyright © 2019 Nouman Pervez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
